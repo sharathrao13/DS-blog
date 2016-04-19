2f9a7153d71881db54202b368b78451fb58bf9ea
suraj-rajesh <surajx.rajesh@gmail.com>
2016-04-19 01:43:27 -0700

    Cleaning master

--- src/server.py
+++ src/server.py
@@ -9,14 +9,9 @@ class Server(object):
         self.sid = sid
         self.clock = 0
         self.n = int(getConfiguration("GeneralConfig", "nodes"))
-<<<<<<< HEAD
-        self.timeTable = [[0 for i in range(self.n)] for i in range(self.n)]
-        self.log = [[] for i in range(self.n)]
-=======
         # log: list of lists. Each list corresponds to each node
         self.log = [[] for i in range(self.n)]
         self.timeTable = [[0 for i in range(self.n)] for i in range(self.n)]
->>>>>>> suraj1
         self.peers = initialize(self.sid, self.n)
         # New server socket to listen to requests from Client/Peers
         self.serverSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
