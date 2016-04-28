1a8f67b6bc73526dc228323d10d0836d52e745a6
Sharath Rao <sharathrao13@gmail.com>
2016-04-28 09:52:52 -0700

    Fix bug

--- server/server.py
+++ server/server.py
@@ -6,8 +6,6 @@ from network_interface import sendObject, receiveObject
 
 
 class Server(object):
-
-
     def __init__(self):
 
         config_reader = ConfigReader('../config.ini')
@@ -18,8 +16,8 @@ class Server(object):
         self.total_nodes = int(config_reader.getConfiguration("GeneralConfig", "nodes"))
         self.peers = config_reader.get_peer_servers(self.current_server_id, self.total_nodes)
 
-        # log: list of lists. Each list corresponds to each node
-        self.log = [[] for i in range(self.total_nodes)]
+        # log: Each list corresponds to each node
+        self.log = list()
         self.timeTable = [[0 for i in range(self.total_nodes)] for i in range(self.total_nodes)]
 
         # Blogs at this server
@@ -56,7 +54,7 @@ class Server(object):
             print "Msg: %s" % msg
             print "Clock: %s" % self.clock
 
-        #Client sends the message sync so that the given server syncs with the given parameter
+        # Client sends the message sync so that the given server syncs with the given parameter
         elif "sync" == msg.operation:
 
             # all the entries where hasRec is false
@@ -64,25 +62,27 @@ class Server(object):
             message = Message(operation="server_sync", timeTable=self.timeTable, logs=filtered_log,
                               sent_server_id=self.current_server_id)
 
-            sendObject(server_socket, message)
+            sock_to_other = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock_to_other.connect(self.config_reader.get_ip_and_port_of_server("Server" + msg.server_to_sync))
+            sendObject(sock_to_other, message)
+            sock_to_other.close()
 
             print "Filtered Log: %s" % self.filtered_log
             print "TT: %s" % self.timeTable
             print "Msg: %s" % msg
             print "Clock: %s" % self.clock
 
-        #Received from a server. Update the logs and sync servers to complete replication
+        # Received from a server. Update the logs and sync servers to complete replication
         elif "server_sync" == msg.operation:
 
             sent_server = msg.sent_server_id
             other_log = msg.logs
             other_time_table = msg.timeTable
 
-            self.add_new_events_to_log(other_time_table)
-            self.update_max_elements(other_time_table, sent_server)
+            self.add_new_events_to_log_and_blog(other_time_table, other_log)
+            self.update_max_elements(other_time_table)
             self.update_rows(other_time_table, sent_server)
 
-            self.update_dictionary()
 
             print "Log: %s" % self.log
             print "TT: %s" % self.timeTable
@@ -103,21 +103,32 @@ class Server(object):
     def filter_log(self, log, server_id):
         pass
 
-    def update_max_elements(self, other_time_table, sent_server):
+    def update_max_elements(self, other_time_table):
+        for i in range(0, self.total_nodes):
+            for j in range(0, self.total_nodes):
+                self.timeTable[i][j] = max(self.timeTable[i][j], other_time_table[i][j])
+
+    def update_rows(self, other_time_table, sent_server):
+        for i in range(0, self.total_nodes):
+            self.timeTable[self.current_server_id][i] = max(self.timeTable[self.current_server_id][i],
+                                                            other_time_table[sent_server][i])
+
+    def has_rec(self, server_to_send):
+        # TT[i][k] = t => Site i knows that event k took place at latest of time t
+        return self.timeTable[self.current_server_id][server_to_send] > time(e)
+
+    def add_new_events_to_log_and_blog(self, other_time_table, other_log):
+        #Add to Log
+        #Add to Dictionary
         pass
 
-    def update_rows(self,other_time_table, sent_server):
-        pass
-
-    def update_dictionary(self):
-        pass
 
 if __name__ == "__main__":
 
     try:
         current_server = Server()
         print "Starting server %s (of total %s) with peers %s" % (
-        current_server.current_server_id, current_server.total_nodes, current_server.peers)
+            current_server.current_server_id, current_server.total_nodes, current_server.peers)
         current_server.start()
 
     except Exception as details:
